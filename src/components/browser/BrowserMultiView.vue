3da6b2e1da4485189849f7030c0798895eb92657
이유로 <eurohand@naver.com>
2023-11-17 00:47:35 +0900

    fest: browser view aspect ratio

--- src/components/browser/BrowserMultiView.vue
+++ src/components/browser/BrowserMultiView.vue
@@ -1,7 +1,68 @@
 <script setup></script>
 
 <template>
-  <div class="w-full h-96 bg-slate-200">싱글</div>
+  <div class="w-full h-96 bg-slate-200 grid grid-cols-3">
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-square bg-cover bg-center"
+    />
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/browser/BrowserSingleView.vue
+++ src/components/browser/BrowserSingleView.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div
-    class="w-full h-96 flex items-center justify-between p-2 relative bg-slate-200 border-[1px] border-slate-950"
+    class="w-full flex items-center justify-between relative bg-slate-200 border-[1px] border-slate-950"
   >
-    <div class="text-lg text-slate-300">&lt;</div>
-    <div class="text-lg text-slate-300">&gt;</div>
-    <div
-      style="background: url('@/assets/attraction.jpg')"
-      class="absolute w-full h-full left-0 top-0 bg-cover bg-center"
-    ></div>
+    <div class="absolute left-2 text-lg text-slate-300 z-10">&lt;</div>
+    <div class="absolute right-2 text-lg text-slate-300 z-10">&gt;</div>
+    <img
+      src="@/assets/attraction.jpg"
+      class="w-full aspect-video bg-cover bg-center"
+    />
   </div>
 </template>
 
